36a24ab758da02a32f175716983aa4211110d350
harrisonthayer <harrisonthayer04@gmail.com>
2025-08-14 15:52:55 -0700

    chore(git): ignore template/history/next.nosync and stop tracking

--- .next.nosync/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WOQflAe3mttzuCXmP2leKq5P87Mt2ON2EGn917ihcHI=\"\n}"
--- .next.nosync/static/chunks/src_app_page_tsx_b614cb15._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_a323f3f7._.js"
-],
-    source: "dynamic"
-});
